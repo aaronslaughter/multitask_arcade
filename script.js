9b071cd190d9bc07f420137a6913bcbfb2da9266
Aaron Slaughter <aslaugh1@gmail.com>
2021-11-19 18:17:44 -0500

    added win conditions, simple ai, and reset game loop

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ class ConnectFour
     this.initListeners();
 
     this.playersTurn = true;
+    this.gameOver = false;
   }
 
   initGrid = () =>
@@ -65,17 +66,23 @@ class ConnectFour
     {
       for (let j = 0; j < connectFourConstants.NUM_COLS; j++)
       {
-        this.buttons[i][j].addEventListener('click', () => 
+        this.buttons[i][j].addEventListener('click', () =>
         {
-          if (!(this.findLowestBlankRow(j) < 0))
+          
+          let lowestBlankRow = this.findLowestBlankRow(j);
+          if (!(lowestBlankRow < 0) && !this.gameOver)
           {
-            if (this.grid[this.findLowestBlankRow(j)][j] === connectFourConstants.BLANK && this.playersTurn)
+            if (this.playersTurn)
             {
-            this.buttons[this.findLowestBlankRow(j)][j].style.backgroundColor = 'red';
-            this.grid[this.findLowestBlankRow(j)][j] = connectFourConstants.RED;
-            // TODO check win conditions
-            this.playersTurn = false;
-            this.computerTurn();
+              this.buttons[lowestBlankRow][j].style.backgroundColor = 'red';
+              this.grid[lowestBlankRow][j] = connectFourConstants.RED;
+              this.playersTurn = false;
+              
+              // checkWin will flip the gameOver boolean if a win is found.
+              if (this.checkWin(this.grid[lowestBlankRow][j]));
+              {
+                this.computerTurn();
+              }
             }
           }
         })
@@ -100,17 +107,132 @@ class ConnectFour
 
   computerTurn = () =>
   {
-    let randomColumn = Math.floor(Math.random() * connectFourConstants.NUM_COLS);
-
-    while(this.findLowestBlankRow(randomColumn) < 0)
+    if (!this.gameOver)
     {
-      randomColumn = Math.floor(Math.random() * connectFourConstants.NUM_COLS);
+      let randomColumn = Math.floor(Math.random() * connectFourConstants.NUM_COLS);
+
+      while(this.findLowestBlankRow(randomColumn) < 0)
+      {
+        randomColumn = Math.floor(Math.random() * connectFourConstants.NUM_COLS);
+      }
+  
+      let lowestBlankRow = this.findLowestBlankRow(randomColumn);
+  
+      this.buttons[lowestBlankRow][randomColumn].style.backgroundColor = 'black';
+      this.grid[lowestBlankRow][randomColumn] = connectFourConstants.BLACK;
+      this.playersTurn = true;
+      this.checkWin(this.grid[lowestBlankRow][randomColumn]);
+    }
+  }
+
+  checkWin = (potentialWinner) =>
+  {
+    // checks the verticals
+    for (let i = 0; i < connectFourConstants.NUM_ROWS-3; i++)
+    {
+      for (let j = 0; j < connectFourConstants.NUM_COLS; j++)
+      {
+        if (this.grid[i][j] === potentialWinner &&
+        this.grid[i+1][j] === potentialWinner &&
+        this.grid[i+2][j] === potentialWinner &&
+        this.grid[i+3][j] === potentialWinner)
+        {
+          this.gameOver = true;
+          this.resetGame([this.buttons[i][j], this.buttons[i+1][j], this.buttons[i+2][j], this.buttons[i+3][j]]);
+          return true;
+        }
+      }
     }
 
-    this.buttons[this.findLowestBlankRow(randomColumn)][randomColumn].style.backgroundColor = 'black';
-    this.grid[this.findLowestBlankRow(randomColumn)][randomColumn] = connectFourConstants.BLACK;
-    // TODO check win conditions
-    this.playersTurn = true;
+    // checks the horizontals
+    for (let i = 0; i < connectFourConstants.NUM_ROWS; i++)
+    {
+      for (let j = 0; j < connectFourConstants.NUM_COLS-3; j++)
+      {
+        if (this.grid[i][j] === potentialWinner &&
+        this.grid[i][j + 1] === potentialWinner &&
+        this.grid[i][j + 2] === potentialWinner &&
+        this.grid[i][j + 3] === potentialWinner)
+        {
+          this.gameOver = true;
+          this.resetGame([this.buttons[i][j], this.buttons[i][j+1], this.buttons[i][j+2], this.buttons[i][j+3]]);
+          return true;
+        }
+      }
+    }
+
+    // checks the ascending diagonals, 'ascending' from the perspective of the origin in the top left
+    for (let i = 0; i < connectFourConstants.NUM_ROWS-3; i++)
+    {
+      for (let j = 0; j < connectFourConstants.NUM_COLS-3; j++)
+      {
+        if (this.grid[i][j] === potentialWinner &&
+        this.grid[i + 1][j + 1] === potentialWinner &&
+        this.grid[i + 2][j + 2] === potentialWinner &&
+        this.grid[i + 3][j + 3] === potentialWinner)
+        {
+          this.gameOver = true;
+          this.resetGame([this.buttons[i][j], this.buttons[i+1][j+1], this.buttons[i+2][j+2], this.buttons[i+3][j+3]]);
+          return true;
+        }
+      }
+    }
+
+    // checked the descending diagonals, 'descending' from the perspective of the origin in the top left
+    for (let i = 3; i < connectFourConstants.NUM_ROWS; i++)
+    {
+      for (let j = 0; j < connectFourConstants.NUM_COLS-3; j++)
+      {
+        if (this.grid[i][j] === potentialWinner &&
+        this.grid[i - 1][j + 1] === potentialWinner &&
+        this.grid[i - 2][j + 2] === potentialWinner &&
+        this.grid[i - 3][j + 3] === potentialWinner)
+        {
+          this.gameOver = true;
+          this.resetGame([this.buttons[i][j], this.buttons[i-1][j+1], this.buttons[i-2][j+2], this.buttons[i-3][j+3]]);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  resetGame = (winningSlots) =>
+  {
+
+
+
+    for (let i = 0; i < 4; i++)
+    {
+      window.setTimeout(() =>
+      {
+        if (!this.playersTurn)
+        {
+          winningSlots[i].style.backgroundColor = 'pink';
+        }
+        else
+        {
+          winningSlots[i].style.backgroundColor = 'green';
+        }
+      }, 500 * i)
+      
+    }
+
+    window.setTimeout(() => 
+    {
+      for (let i = 0; i < connectFourConstants.NUM_ROWS; i++)
+      {
+        for (let j = 0; j < connectFourConstants.NUM_COLS; j++)
+        {
+          this.grid[i][j] = connectFourConstants.BLANK;
+          this.buttons[i][j].style.backgroundColor = '';
+        }
+      }
+
+      this.gameOver = false;
+      this.playersTurn = true;
+    }, 4000)
   }
 
 }
