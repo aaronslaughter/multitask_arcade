dbbb5ff416333f71485d8703f8e4b9485deff297
Aaron Slaughter <aslaugh1@gmail.com>
2021-11-22 15:26:51 -0500

    created Math Class game

--- script.js
+++ script.js
@@ -12,19 +12,17 @@ Object.freeze(connectFourConstants);
 
 class ConnectFour
 {
+  grid = new Array(connectFourConstants.NUM_ROWS);
+  buttons = new Array(connectFourConstants.NUM_ROWS);
+  playersTurn = true;
+  gameOver = false;
+  secondsRemaining = 10;
+
   constructor()
-  {
-    this.grid = new Array(connectFourConstants.NUM_ROWS);
-    this.initGrid();
-
-    this.buttons = new Array(connectFourConstants.NUM_ROWS);
+  {    
+    this.initGrid();  
     this.initButtons();
-
     this.initListeners();
-
-    this.playersTurn = true;
-    this.gameOver = false;
-    this.secondsRemaining = 10;
 
     this.turnTimer = window.setInterval(() => 
     {
@@ -339,5 +337,179 @@ class ConnectFour
   }
 }
 
+/////////////////////////////////
+
+///////// MATH CLASS ////////////
+
+class MathClass {
+  parameter1;
+  parameter2;
+  operators = ['+', '-'];
+  currentOperator;
+  correctAnswer;
+  correctButton;
+  wrongAnswers = new Array();
+  buttons = new Array();
+  range = 10;
+  equation = new Array();
+  roundOver = true;
+
+  constructor()
+  {
+    this.initButtons();
+    this.initListeners();
+    this.initEquation();
+    this.createEquation();
+    this.displayEquation();
+  }
+
+  initButtons()
+  {
+    for (let i = 0; i < document.querySelectorAll('.answer-button').length; i++)
+    {
+      this.buttons.push(document.querySelectorAll('.answer-button')[i]);
+    }
+  }
+
+  initListeners()
+  {
+    for (let i = 0; i < this.buttons.length; i++)
+    {
+      this.buttons[i].addEventListener('click', () =>
+      {
+        // TODO
+        // check answer is correct
+        // award points, etc.
+
+        if (this.buttons[i] === this.correctButton && !this.roundOver)
+        {
+          this.roundOver = true;
+          this.buttons[i].style.backgroundColor = 'green';
+          window.setTimeout( () => {this.resetBoard()}, 2000);
+        }
+        else if (this.buttons[i] != this.correctButton && !this.roundOver)
+        {
+          this.roundOver = true;
+          this.buttons[i].style.backgroundColor = 'red';
+          this.correctButton.style.backgroundColor = 'green';
+          window.setTimeout( () => {this.resetBoard()}, 2000);
+          
+        }
+      })
+    }
+  }
+
+  initEquation()
+  {
+    for (let i = 0; i < 5; i++)
+    {
+      this.equation.push(document.querySelectorAll('.equation')[i]);
+    }
+  }
+
+  createEquation()
+  {
+    this.parameter1 = Math.floor((Math.random() * (this.range * 2)) - this.range);
+    if (this.parameter1 === 0)
+    {
+      this.parameter1++;
+    }
+
+    this.parameter2 = Math.floor((Math.random() * (this.range * 2)) - this.range);
+    if (this.parameter2 === 0)
+    {
+      this.parameter2++;
+    }
+
+    if (this.parameter1 === this.parameter2 || Math.abs(this.parameter1) === (Math.abs(this.parameter2)))
+    {
+      this.parameter2++;
+    }
+
+    this.currentOperator = this.operators[Math.floor(Math.random() * 2)]
+
+    if (this.currentOperator === '+')
+    {
+      this.correctAnswer = this.parameter1 + this.parameter2;
+    }
+    else if (this.currentOperator === '-')
+    {
+      this.correctAnswer = this.parameter1 - this.parameter2;
+    }
+
+    if (this.currentOperator === '+') // -2
+    {
+      this.wrongAnswers.push(this.parameter1 + (this.parameter2 * -1)); // 16
+      this.wrongAnswers.push((this.parameter1 * -1) + this.parameter2); // 
+      this.wrongAnswers.push((this.parameter1 * -1) + (this.parameter2 * -1));
+    }
+    else if (this.currentOperator === '-')
+    {
+      this.wrongAnswers.push(this.parameter1 - (this.parameter2 * -1)); // -1
+      this.wrongAnswers.push((this.parameter1 * -1) - this.parameter2); // 1
+      this.wrongAnswers.push((this.parameter1 * -1) - (this.parameter2 * -1)); 
+    }
+
+    console.log(this.wrongAnswers);
+  }
+
+  displayEquation()
+  {
+    window.setTimeout( () => {this.equation[0].innerText = this.parameter1}, 500);
+    window.setTimeout( () => {this.equation[1].innerText = this.currentOperator}, 1500);
+    window.setTimeout( () => {this.equation[2].innerText = this.parameter2}, 2500);
+    window.setTimeout( () => {this.displayAnswers()}, 3500);
+  }
+
+  displayAnswers()
+  {
+    let usedWrongAnswerIndexes = [];
+    let answerIndex = Math.floor(Math.random() * 4);
+
+    this.buttons[answerIndex].innerText = this.correctAnswer;
+    this.correctButton = this.buttons[answerIndex];
+
+    for (let i = 0; i < 4; i++)
+    {
+      if (i != answerIndex)
+      {
+        let randomIndex = Math.floor(Math.random() * 3);
+        
+        while (usedWrongAnswerIndexes.includes(randomIndex))
+        {
+          randomIndex = Math.floor(Math.random() * 3);
+        }
+
+        this.buttons[i].innerText = this.wrongAnswers[randomIndex];
+        usedWrongAnswerIndexes.push(randomIndex);
+      }
+    }
+
+    this.range ++;
+    this.roundOver = false;
+    
+  }
+
+  resetBoard()
+  {
+    this.equation[0].innerText = '';
+    this.equation[1].innerText = '';
+    this.equation[2].innerText = '';
+
+    this.wrongAnswers = [];
+
+    for (let i = 0; i < this.buttons.length; i++)
+    {
+      this.buttons[i].innerText = '';
+      this.buttons[i].style.backgroundColor = '';
+    }
+
+    this.createEquation();
+    this.displayEquation();
+    
+  }
+}
+
 const connectFour = new ConnectFour();
+const mathClass = new MathClass();
 
